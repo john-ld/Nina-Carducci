6d4dbd9535a4c5cb3f41f9267707489528c8e9d3
john-ld <153510178+john-ld@users.noreply.github.com>
2024-10-09 20:49:25 +0200

    minification style css

---
+++ ninacarducci.github.io-main/assets/purgecss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        './ninacarducci.github.io-main/index.html',
+        './ninacarducci.github.io-main/assets/**/*.js'
+    ],
+    css: [
+        './ninacarducci.github.io-main/assets/bootstrap/bootstrap.min.css',
+        './ninacarducci.github.io-main/assets/style.css'
+    ],
+    output: './ninacarducci.github.io-main/assets/css'
+};
